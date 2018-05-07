5ae4835630adeb67627ad878237211766c19de52
Greeshma Pulinkuzhy Balan <greeshma.balan@ad.infosys.com>
2018-05-07 17:00:48 -0400

    added the calculator functionality to the project

---
+++ InterestcalcProject/InterestcalcProject/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterestcalcProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Calculate()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(float amount, float years,float rate)
+        {
+            float interest = 0;
+            interest = amount * years * (rate / 100);
+            ViewBag.Interest = interest;
+            ViewBag.Message = "Interest calculated successfully";
+            return View("Calculate");
+        }
+    }
+}
---
+++ InterestcalcProject/InterestcalcProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InterestcalcProject.Startup))]
+namespace InterestcalcProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
