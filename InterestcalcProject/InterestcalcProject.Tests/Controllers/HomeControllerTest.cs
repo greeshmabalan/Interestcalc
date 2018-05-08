356f1ceaa7288ab2e57eba7537e435263a2386fe
Greeshma Pulinkuzhy Balan <greeshma.balan@ad.infosys.com>
2018-05-08 16:57:14 -0400

    new

--- InterestcalcProject/InterestcalcProject.Tests/Controllers/HomeControllerTest.cs
+++ InterestcalcProject/InterestcalcProject.Tests/Controllers/HomeControllerTest.cs
@@ -35,7 +35,7 @@ namespace InterestcalcProject.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.AreEqual("Your application description.", result.ViewBag.Message);
         }
 
         [TestMethod]
